334f32687863ec336b94d254692b1d8276d3dfa3
cheyannemanivong <36451200+cheyannemanivong@users.noreply.github.com>
2021-05-06 14:57:54 -0400

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,94 @@
+API_KEY = 'insert your token here'
+
+
+# switch between languages -> comment out whichever version you're not using
+
+# english version
+cancelString = 'Send /cancel to end command or start over.'
+
+helpMenu = 'Here are the available commands for this bot:\n' + \
+           '/driver - Change the name of a vehicle\'s driver.\n' + \
+           '/plate - Change the license plate of a vehicle.\n' + \
+           '/refuel - Begin the refueling procedure.'
+
+driverString = 'Select your vehicle. \n\n' + cancelString
+
+selectVehicleDriverString1 = 'Changing driver name for vehicle '
+selectVehicleDriverString2 = '\n\nEnter the name.\n\n' + \
+                             cancelString
+
+thankYou = 'Thank you.'
+
+plateString = 'Select your vehicle. \n\n' + \
+              cancelString
+
+selectVehiclePlateString1 = 'Changing license plate number for vehicle '
+selectVehiclePlateString2 = '\n\nEnter the new plate number.\n\n' + \
+                            cancelString
+
+refuelString = 'Select your vehicle. \n\n' + \
+               cancelString
+
+selectVehicleRefuelString1 = 'Refueling for vehicle '
+selectVehicleRefuelString2 = '\n\nBefore refueling, enter the mileage.\n\n' + \
+                             cancelString
+
+mileageBeforeString = 'After refueling, enter the mileage.\n\n' + \
+                      cancelString
+
+mileageAfterString = 'Enter the quantity of fuel (in liters).\n\n' + \
+                     cancelString
+
+refuelQuantityString = 'Enter the cost.\n\n' + \
+                       cancelString
+
+refuelCostString = 'Thank you. Don\'t forget to deliver all bills and photos to the coordinator. \n'
+
+commandStopped = 'Command stopped.'
+
+
+# # portugese version
+# cancelString = 'Enviar /cancel para terminar o comando ou reiniciar.'
+#
+# helpMenu = 'Aqui estão os comandos disponíveis para este bot:\n' + \
+#            '/driver - Alterar o nome do motorista de um veículo.\n' + \
+#            '/plate - Alterar a placa de um veículo.\n' + \
+#            '/refuel - Comece o procedimento de reabastecimento.'
+#
+# driverString = 'Selecione seu veículo. \n\n' + \
+#                cancelString
+#
+# selectVehicleDriverString1 = 'Alterar o nome do motorista do veículo '
+# selectVehicleDriverString2 = '\n\nDigite o nome.\n\n' + \
+#                              cancelString
+#
+# thankYou = 'Obrigada.'
+#
+# plateString = 'Selecione seu veículo. \n\n'+ \
+#               cancelString
+#
+# selectVehiclePlateString1 = 'Alteração do número da placa do veículo '
+# selectVehiclePlateString2 = '\n\nInsira o novo número da placa.\n\n' + \
+#                             cancelString
+#
+# refuelString = 'Selecione seu veículo. \n\n' + \
+#                cancelString
+#
+# selectVehicleRefuelString1 = 'Reabastecimento para veículo '
+# selectVehicleRefuelString2 = '\n\nAntes de reabastecer, insira a quilometragem.\n\n' + \
+#                              cancelString
+#
+# mileageBeforeString = 'Após o reabastecimento, insira a quilometragem.\n\n' + \
+#                       cancelString
+#
+# mileageAfterString = 'Insira a quantidade de combustível (em litros).\n\n' + \
+#                      cancelString
+#
+# refuelQuantityString = 'Insira o custo.\n\n' + \
+#                        cancelString
+#
+# refuelCostString = 'Obrigada. Não se esqueça de entregar todas as contas e fotos ao coordenador.\n'
+#
+# commandStopped = 'O comando parou.'
+
+
